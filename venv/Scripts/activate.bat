bd93546d59bc8a686a4d46827d8542d585692ae1
VishnuSree <vchit7@uic.edu>
2024-11-12 17:01:35 -0600

    Fix virtual environment and regenerate requirements.txt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\UICSem1\IntroToDataScience\class-project-cheesy-little-explorers\venv
+set VIRTUAL_ENV=C:\Users\HP\Desktop\418 Intro DS\Project\class-project-cheesy-little-explorers\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
